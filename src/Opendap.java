3073158b419e5f151bce966b1827464f02e5f6be
Bob <seedor87@students.rowan.edu>
2016-06-30 14:14:38 -0400

    Added specification of date range to extract info for u and v

--- src/Opendap.java
+++ src/Opendap.java
@@ -6,6 +6,8 @@ import java.net.*;
 import java.io.*;
 import java.awt.Desktop;
 import java.util.ArrayList;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
@@ -20,10 +22,20 @@ public class Opendap {
             URL base = new URL("http://podaac-opendap.jpl.nasa.gov/opendap/allData/oscar/preview/L4/oscar_third_deg/");
             URL url = new URL(base, buildFileName(args[2]));
 
-            openHtmlURL(url.toString());
+            //openHtmlURL(url.toString());
 
-            getU(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-            getV(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+            int day = Integer.parseInt(args[0]);
+            int mo = Integer.parseInt(args[1]);
+            int year = Integer.parseInt(args[2]);
+
+            String timeVal = findTimeVal(day, mo, year);
+            print(timeVal);
+
+            String concatUrl = url.toString().substring(0, url.toString().length()-4) + "ascii?u" + timeVal + "[0:1:0][0:1:480][0:1:1200]";
+            openHtmlURL(concatUrl);
+
+            print(findDaysBetween("5.10.1992", "" + day + "." + mo + "." + year));
+
         }
         catch (IOException e) {
             e.printStackTrace();
@@ -32,6 +44,26 @@ public class Opendap {
 
     private static String buildFileName(String name) {
         return "oscar_vel" + name + ".nc.gz.html";
+    }
+
+    private static String findTimeVal(int day, int mo, int year) {
+        int daysBewteen = findDaysBetween("1.1." + year, "" + day + "." + mo + "." + year);
+        return "[" + (daysBewteen / 5) + ":1:71]";
+    }
+
+    public static int findDaysBetween(String start, String end) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        long diff = -1;
+        try {
+            Date dateStart = simpleDateFormat.parse(start);
+            Date dateEnd = simpleDateFormat.parse(end);
+
+            //time is always 00:00:00 so rounding should help to ignore the missing hour when going from winter to summer time as well as the extra hour in the other direction
+            diff = Math.round((dateEnd.getTime() - dateStart.getTime()) / (double) 86400000);
+        } catch (Exception e) {
+            //handle the exception according to your own situation
+        }
+        return (int) diff;
     }
 
     private static ArrayList getU(int mo, int day) {
