eecb28f93cd8c555ae02ed756d118b9878b7a709
lapost48 <lapost48@gmail.com>
2016-07-01 17:47:30 -0400

    Created a class that extends the DatabaseFasade. This class has an example of how new queries can be added.

---
+++ src/fasade/AisDatabaseFasade.java
@@ -0,0 +1,23 @@
+package fasade;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ * Created by lapost48 on 7/1/2016.
+ */
+public class AisDatabaseFasade extends DatabaseFasade {
+
+    public AisDatabaseFasade() {
+        super();
+    }
+
+    public ResultSet getSpeeds(String id) {
+        String col = columnNames.get("speed");
+        String query = "SELECT " + columnNames.get("time") + "," + col + " FROM " + tableName + " where " + columnNames.get("id") + "=" + id;
+
+        return executeQuery(query);
+    }
+
+}
