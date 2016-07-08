1676869deb844a3c88c4ffe9ecbe0fc40e902fe5
Bob <seedor87@students.rowan.edu>
2016-07-08 18:43:42 -0400

    In progress work on being able to generate any given shape using the necessary start and end, as well as angle of reflection

--- src/curve_generator.java
+++ src/curve_generator.java
@@ -25,24 +25,27 @@ public class curve_generator
      */
     public static void main(String[] args) throws IOException
     {
-        PointF start = new PointF(-10f, 30.0f);
-        PointF end = new PointF(-100f, 0.0f);
-        float angle = 100f;
+        PointF start = new PointF(-100f, 0.0f);
+        PointF dest = new PointF(-50f, 35f);
+        PointF end = new PointF(-110f, 50.0f);
+        PointF end2 = findReflected(end, start, dest);
+        float radius = 30f;
         float period = 3f;
 
         /*
+        THIS IS OF MOST CONCERN
+
         start is start point
         end  is end point
-        angle is float of degrees of flex
-        the first boolean represents convex or concave in this case true is concave with respect to north
-        the next boolean represents the side of inflection
-        angle is the angle of reflection
-        period is the relative frequency of the points
+        radius is float of degrees of flex
+        the first boolean represents the choice of shortest curve or longest
+        next boolean represents the side of inflection false is up ward curving, true is downward
         */
-        List<PointF> points = generateCurve(start, end, angle, period, true, false);
+        List<PointF> points = generateCurve(start, end, radius, period, true, false);
 
         System.out.println(points);
 
+        //UNCOMMENT THE FOLLOWING TO YIELD IMAGE
         // Calculate the bounds of the curve
         Rectangle2D.Float bounds = new Rectangle2D.Float(points.get(0).x, points.get(0).y, 0, 0);
         for (int i = 1; i < points.size(); ++i) {
@@ -68,19 +71,37 @@ public class curve_generator
             if (i > 0) {
                 Line2D.Float f = new Line2D.Float(points.get(i - 1).x, points.get(i - 1).y, points.get(i).x, points.get(i).y);
                 System.out.println("Dist : " + f.getP1().distance(f.getP2()));
-//                g.draw(f);
+                g.draw(f);
             }
 
             g.fill(new Ellipse2D.Float(points.get(i).x - 0.8f, points.get(i).y - 0.8f, 1.6f, 1.6f));
 
         }
+
+        g.setColor(Color.GREEN);
+        Line2D.Float f = new Line2D.Float(start.x, start.y, dest.x, dest.y);
+        System.out.println("Dist : " + f.getP1().distance(f.getP2()));
+        g.draw(f);
+
         g.setColor(Color.BLUE);
         g.fill(new Ellipse2D.Float(start.x - 1, start.y - 1, 3, 3));
         g.fill(new Ellipse2D.Float(end.x - 1, end.y - 1, 3, 3));
+        g.fill(new Ellipse2D.Float(dest.x - 1, dest.y -1, 3,3));
+
 
         g.dispose();
 
         ImageIO.write(img, "PNG", new File("result.png"));
+    }
+
+    static PointF findReflected(PointF focus, PointF start, PointF end) {
+        float x;
+        float y;
+
+        //first find eq of line start to end
+
+
+        return new PointF(x, y);
     }
 
     static class PointF
@@ -101,46 +122,46 @@ public class curve_generator
         }
     }
 
-    private static List<PointF> generateCurve(PointF pFrom, PointF pTo, float pRadius, float pMinDistance, boolean shortest, boolean side)
+    private static List<PointF> generateCurve(PointF start, PointF end, float radius, float min_dist, boolean shortest, boolean side)
     {
 
         List<PointF> pOutPut = new ArrayList<PointF>();
 
         // Calculate the middle of the two given points.
-        PointF mPoint = new PointF(pFrom.x + pTo.x, pFrom.y + pTo.y);
+        PointF mPoint = new PointF(start.x + end.x, start.y + end.y);
         mPoint.x /= 2.0f;
         mPoint.y /= 2.0f;
         System.out.println("Middle Between From and To = " + mPoint);
 
 
         // Calculate the distance between the two points
-        float xDiff = pTo.x - pFrom.x;
-        float yDiff = pTo.y - pFrom.y;
+        float xDiff = end.x - start.x;
+        float yDiff = end.y - start.y;
         float distance = (float) Math.sqrt(xDiff * xDiff + yDiff * yDiff);
         System.out.println("Distance between From and To = " + distance);
 
-        if (pRadius * 2.0f < distance) {
+        if (radius * 2.0f < distance) {
             throw new IllegalArgumentException("The radius is too small! The given points wont fall on the circle.");
         }
 
         // Calculate the middle of the expected curve.
-        float factor = (float) Math.sqrt((pRadius * pRadius) / ((pTo.x - pFrom.x) * (pTo.x - pFrom.x) + (pTo.y - pFrom.y) * (pTo.y - pFrom.y)) - 0.25f);
+        float factor = (float) Math.sqrt((radius * radius) / ((end.x - start.x) * (end.x - start.x) + (end.y - start.y) * (end.y - start.y)) - 0.25f);
         PointF circleMiddlePoint = new PointF(0, 0);
         if (side) {
-            circleMiddlePoint.x = 0.5f * (pFrom.x + pTo.x) + factor * (pTo.y - pFrom.y);
-            circleMiddlePoint.y = 0.5f * (pFrom.y + pTo.y) + factor * (pFrom.x - pTo.x);
+            circleMiddlePoint.x = 0.5f * (start.x + end.x) + factor * (end.y - start.y);
+            circleMiddlePoint.y = 0.5f * (start.y + end.y) + factor * (start.x - end.x);
         } else {
-            circleMiddlePoint.x = 0.5f * (pFrom.x + pTo.x) - factor * (pTo.y - pFrom.y);
-            circleMiddlePoint.y = 0.5f * (pFrom.y + pTo.y) - factor * (pFrom.x - pTo.x);
+            circleMiddlePoint.x = 0.5f * (start.x + end.x) - factor * (end.y - start.y);
+            circleMiddlePoint.y = 0.5f * (start.y + end.y) - factor * (start.x - end.x);
         }
         System.out.println("Middle = " + circleMiddlePoint);
 
         // Calculate the two reference angles
-        float angle1 = (float) Math.atan2(pFrom.y - circleMiddlePoint.y, pFrom.x - circleMiddlePoint.x);
-        float angle2 = (float) Math.atan2(pTo.y - circleMiddlePoint.y, pTo.x - circleMiddlePoint.x);
+        float angle1 = (float) Math.atan2(start.y - circleMiddlePoint.y, start.x - circleMiddlePoint.x);
+        float angle2 = (float) Math.atan2(end.y - circleMiddlePoint.y, end.x - circleMiddlePoint.x);
 
         // Calculate the step.
-        float step = pMinDistance / pRadius;
+        float step = min_dist / radius;
         System.out.println("Step = " + step);
 
         // Swap them if needed
@@ -161,13 +182,13 @@ public class curve_generator
             }
         }
         for (float f = angle1; f < angle2; f += step) {
-            PointF p = new PointF((float) Math.cos(f) * pRadius + circleMiddlePoint.x, (float) Math.sin(f) * pRadius + circleMiddlePoint.y);
+            PointF p = new PointF((float) Math.cos(f) * radius + circleMiddlePoint.x, (float) Math.sin(f) * radius + circleMiddlePoint.y);
             pOutPut.add(p);
         }
         if (flipped ^ side) {
-            pOutPut.add(pFrom);
+            pOutPut.add(start);
         } else {
-            pOutPut.add(pTo);
+            pOutPut.add(end);
         }
 
         return pOutPut;
