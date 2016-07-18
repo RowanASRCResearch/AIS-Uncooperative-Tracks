8489d7530bc9b695a1d0d07d1126bc9394fed024
eliakah <kakoue66@students.rowan.edu>
2016-07-18 13:30:45 -0400

    added instructions regarding last step.

--- src/gathering/Scraper.java
+++ src/gathering/Scraper.java
@@ -10,30 +10,32 @@ import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Research on 6/29/2016.
  */
 //TODO: make tide info into JSON
 public class Scraper {
+    File file;
+    String fileName;
+
+    public Scraper(String url, String fileName) throws IOException, InterruptedException {
+        download(url, fileName);
+        file = new File(fileName);
+    }
+
     /**
      * main.
      *
      * @param args the input arguments
      * @throws IOException the io exception
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Scraper scraper = new Scraper("http://tidesandcurrents.noaa.gov/stationhome.html?id=9455090", "tide.html");
-        System.out.println(scraper.getTagContent("div", "div"));
+        System.out.println(scraper.getTagContent("div class=\"row-fluid\"", "div"));
         //scraper.download("http://www.ndbc.noaa.gov/kml/marineobs_as_kml.php?sort=owner","testfile.kml");   scraper.download();
 
-    }
-
-    File file ;
-    String fileName;
-    public Scraper(String url, String fileName) throws IOException {
-        download(url, fileName);
-        file = new File(fileName);
     }
 
     /**
@@ -45,10 +47,11 @@ public class Scraper {
      * @return the boolean
      * @throws IOException the io exception
      */
-    boolean download(String address, String fName) throws IOException {
+    boolean download(String address, String fName) throws IOException, InterruptedException {
 
         URL website = new URL(address);
-        ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+        InputStream stream = website.openStream();
+        ReadableByteChannel rbc = Channels.newChannel(stream);
         FileOutputStream fos = new FileOutputStream(fName);
         fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
         return true;
@@ -61,11 +64,13 @@ public class Scraper {
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
             lineFromFile = scanner.nextLine(); //puts line in String
-            if (lineFromFile.contains("<"+tag)) { //check if line contains tag
+            if (lineFromFile.contains("<" + tag + ">")) { //check if line contains tag
+                str += "\n" + lineFromFile;
                 check = true;
                 while (scanner.hasNextLine()) {
                     lineFromFile = scanner.nextLine();
                     if (lineFromFile.contains("</"+endTag+">")) {
+                        str += "\n" + lineFromFile;
                         break;
                     } else {
 
