03cacec5a6f06b64dacca43822c3f40c7c26a814
lapost48 <lapost48@gmail.com>
2016-08-29 12:06:48 -0400

    Fixed naming issue with weather vectors

---
+++ src/plotting/WindVector.java
@@ -0,0 +1,18 @@
+package plotting;
+
+import prediction.Point;
+
+/**
+ * Created by Research on 8/17/2016.
+ */
+public class WindVector extends WeatherVector {
+
+    public WindVector(Point location, float latComponent, float longComponent) {
+        super(location, latComponent, longComponent);
+    }
+
+    public String toString() {
+        return "Wind Vector\n" + super.toString();
+    }
+
+}
